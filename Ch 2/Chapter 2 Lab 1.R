c645b6da8fb922d41885a9611e2326930cb6621a
daniel1197cama <daniel.camacho@my.simpson.edu>
2020-01-29 22:34:56 -0600

    Ch 2 Problem 9 (Final Version)

---
+++ Ch 2/Chapter 2 Lab 1.R
@@ -0,0 +1,10 @@
+# Question 9 in Chaoter 2 in ISLR
+require(ISLR)
+myAuto <- Auto
+# mpg is the outpout variable
+# all others are predictors
+
+names(Auto)
+summary(Auto)
+str(Auto)
+# 9.a Origin and name are qualitative and all others are quantitavitve.
